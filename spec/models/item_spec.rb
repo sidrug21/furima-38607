5ba8f40a2bd8951f2f1dd5afb83001976df10e85
sidrug21 <sidrug.beth21@gmail.com>
2023-03-31 21:05:15 +0900

    商品出品機能の修正_2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 RSpec.describe Item, type: :model do
   before do
-    @user = FactoryBot.create(:user)
-    @item = FactoryBot.build(:item, user_id: @user.id)
+    @item = FactoryBot.build(:item)
   end
 
   describe '商品出品機能' do
