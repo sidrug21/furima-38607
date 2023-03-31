d024b7ccec48ba6787fc2040049c8d7f3e32320d
sidrug21 <sidrug.beth21@gmail.com>
2023-03-31 19:44:14 +0900

    Rubocopにて自動整形

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true, inclusion: { in: 2..48 }
   validates :shipping_duration_id, presence: true, inclusion: { in: 2..4 }
   validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
   has_one_attached :image
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,31 +28,31 @@ RSpec.describe Item, type: :model do
       it 'カテゴリーが未選択では商品出品できない' do
         @item.category_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category is not included in the list")
+        expect(@item.errors.full_messages).to include('Category is not included in the list')
       end
 
       it '商品の状態が未選択では商品出品できない' do
         @item.condition_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition is not included in the list")
+        expect(@item.errors.full_messages).to include('Condition is not included in the list')
       end
 
       it '配送料の負担が未選択では商品出品できない' do
         @item.shipping_fee_burden_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping fee burden is not included in the list")
+        expect(@item.errors.full_messages).to include('Shipping fee burden is not included in the list')
       end
 
       it '発送元の地域が未選択では商品出品できない' do
         @item.prefecture_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture is not included in the list")
+        expect(@item.errors.full_messages).to include('Prefecture is not included in the list')
       end
 
       it '発送までの日数が未選択では商品出品できない' do
         @item.shipping_duration_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping duration is not included in the list")
+        expect(@item.errors.full_messages).to include('Shipping duration is not included in the list')
       end
 
       it '商品画像がなければ商品出品できない' do
@@ -70,34 +70,32 @@ RSpec.describe Item, type: :model do
       it '価格が半角数字以外では商品出品できない' do
         @item.price = '１０００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
 
       it '価格が半角英数字混合では商品出品できない' do
-        @item.price = "3000yen"
+        @item.price = '3000yen'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
-    
+
       it '価格が半角英字のみでは商品出品できない' do
-        @item.price = "three thousand"
+        @item.price = 'three thousand'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
-    
+
       it '価格が300円未満では商品出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
-    
+
       it '価格が10,000,000円以上では商品出品できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
     end
   end
 end
-    
-     
