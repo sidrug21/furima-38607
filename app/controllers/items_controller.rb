98e76b90ee845f3fa092d7c8c71dc21a2c2725df
sidrug21 <sidrug.beth21@gmail.com>
2023-04-03 14:12:45 +0900

    before_actionメソッドにて可読性の向上

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all
   end
@@ -10,7 +10,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def create
@@ -24,7 +23,7 @@ class ItemsController < ApplicationController
 
   def edit
     if user_signed_in?
-      @item = Item.find(params[:id])
+
       redirect_to(root_path) unless current_user == @item.user
     else
       redirect_to(new_user_session_path)
@@ -32,7 +31,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -45,5 +43,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id,
                                  :shipping_duration_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
